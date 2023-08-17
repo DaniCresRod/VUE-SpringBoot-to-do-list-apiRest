2fa7bda6fddca6d4b231133dcb80c5974de0cdd3
albegosu <albegosu@gmail.com>
2023-08-17 15:27:59 +0200

    Prueba components

--- FRONT/src/App.vue
+++ FRONT/src/App.vue
@@ -1,7 +1,13 @@
-<template>
-  <router-view />
-</template>
-
 <script setup>
-  //
+  import FooterComponent from '@/components/FooterComponent.vue';
 </script>
+
+<template>
+
+  <FooterComponent />
+  <v-card class="mx-auto" width="400" prepend-icon="mdi-home">
+    <template v-slot:title> This is a title </template>
+
+    <v-card-text> This is content </v-card-text>
+  </v-card>
+</template>
