cda7819a98391de3e94925e2f1139f45feb7e8bb
Dani <dani.crespo.rodriguez@gmail.com>
2023-08-17 15:01:25 +0200

    Ok header

--- FRONT/src/views/Home.vue
+++ FRONT/src/views/Home.vue
@@ -1,7 +1,68 @@
-<template>
-  <HelloWorld />
+<template><v-card>
+  <v-layout>     
+
+    <v-app-bar
+      color="primary"
+      prominent
+    >
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-no-wrap" title="Application">Tods List</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      location="top"
+      temporary
+    >
+      <v-list
+        :items="items"
+      ></v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+    <!-- <v-main style="height: 100vh;"> -->
+
+      <HelloWorld />
+
+    </v-main>
+  </v-layout>
+</v-card>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+ import HelloWorld from '@/components/HelloWorld.vue';
+  import { ref, watch } from 'vue';
+
+const drawer = ref(false);
+const group = ref(null);
+
+const items = [
+  {
+    title: 'Foo',
+    value: 'foo',
+  },
+  {
+    title: 'Bar',
+    value: 'bar',
+  },
+  {
+    title: 'Fizz',
+    value: 'fizz',
+  },
+  {
+    title: 'Buzz',
+    value: 'buzz',
+  },
+];
+
+watch(group, () => {
+  drawer.value = false;
+});
 </script>
