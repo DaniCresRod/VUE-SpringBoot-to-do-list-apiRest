efc1cc3a3d855e4db069513ebd7ec1abf2f2deb7
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-08-18 09:35:23 +0200

    Ok controllers structure

--- BBDD-CRUD/src/main/java/com/example/first/api/services/services.java
+++ BBDD-CRUD/src/main/java/com/example/first/api/services/services.java
@@ -1,4 +1,28 @@
 package com.example.first.api.services;
 
+import com.example.first.api.models.models;
+import com.example.first.api.repositories.repositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class services {
+
+    @Autowired
+    repositories myRepo;
+
+
+    public ArrayList<models> getProducts() {
+    }
+
+    public String deleteProduct(int id) {
+    }
+
+    public models setProduct(models newProduct) {
+    }
+
+    public models changeProduct(int id, models newProduct) {
+    }
 }
