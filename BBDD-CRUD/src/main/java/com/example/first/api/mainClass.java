0d9e9c579613b8df8e0ca2b06f1de7a0a56cfc14
Dani <dani.crespo.rodriguez@gmail.com>
2023-08-18 08:41:42 +0200

    WIP struture bd

---
+++ BBDD-CRUD/src/main/java/com/example/first/api/mainClass.java
@@ -0,0 +1,7 @@
+package com.example.first.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mainClass {
+}
--- BBDD-CRUD/src/main/java/com/example/first/api/models/models.java
+++ BBDD-CRUD/src/main/java/com/example/first/api/models/models.java
@@ -1,4 +1,33 @@
 package com.example.first.api.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class models {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String prodName;
+    private enumType prodType ;
+    private String prodMessage;
+    private enumSize prodSize;
+    private String prodColor;
+
+
+    enum enumType{
+        SHIRT,
+        HOODIE,
+        MUG,
+        BOTTLE
+    }
+
+    enum enumSize{
+        S,M,L,XL
+    }
+
+
 }
