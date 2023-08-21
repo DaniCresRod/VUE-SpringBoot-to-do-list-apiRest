bdb047eba52fdd4c7f82d6b81750dd47fde8e867
LuLu <lu.luzan@gmail.com>
2023-08-21 15:33:03 +0200

    refactor fetch

--- FRONT/src/Configuration/ApiService.vue
+++ FRONT/src/Configuration/ApiService.vue
@@ -13,31 +13,31 @@ import { ref, onMounted } from "vue";
 import useFetch from "../components/hooks/useFetch";
 import DynamicUrl from "../components/services/DynamicUrl";
 
-export default {
-    setup() {
-    const data = ref(null);
-    const error = ref(null);
-    const isLoading = ref(true);
+// export default {
+//     setup() {
+//     const data = ref(null);
+//     const error = ref(null);
+//     const isLoading = ref(true);
 
-    onMounted(() => {
-        fetch(`${DynamicUrl}/XXXXX`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(apiData => {
-            data.value = apiData;
-            isLoading.value = false;
-        })
-        .catch(err => {
-            error.value = err;
-            isLoading.value = false;
-        });
-    });
+//     onMounted(() => {
+//         fetch(`${DynamicUrl}/goods`)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error("Network response was not ok");
+//             }
+//             return response.json();
+//         })
+//         .then(apiData => {
+//             data.value = apiData;
+//             isLoading.value = false;
+//         })
+//         .catch(err => {
+//             error.value = err;
+//             isLoading.value = false;
+//         });
+//     });
 
-    return { data, error, isLoading };
-    },
-};
+//     return { data, error, isLoading };
+//     },
+// };
 </script>
--- FRONT/src/components/hooks/useFetch.js
+++ FRONT/src/components/hooks/useFetch.js
@@ -1,7 +1,9 @@
 import { ref, onMounted } from "vue";
 
 const useFetch = (dynamicUrl) => {
-  const data = ref([]);
+  const data = ref([null]);
+  const error = ref(null);
+  const isLoading = ref(true);
   const fetchData = async () => {
     try {
       const response = await fetch(dynamicUrl);
@@ -16,5 +18,32 @@ const useFetch = (dynamicUrl) => {
 
   return data;
 };
+// export default {
+//     setup() {
+//     const data = ref(null);
+    
+
+//     onMounted(() => {
+//         fetch(`${DynamicUrl}/goods`)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error("Network response was not ok");
+//             }
+//             return response.json();
+//         })
+//         .then(apiData => {
+//             data.value = apiData;
+//             isLoading.value = false;
+//         })
+//         .catch(err => {
+//             error.value = err;
+//             isLoading.value = false;
+//         });
+//     });
+
+//     return { data, error, isLoading };
+//     },
+// };
+
 
 export default useFetch;
