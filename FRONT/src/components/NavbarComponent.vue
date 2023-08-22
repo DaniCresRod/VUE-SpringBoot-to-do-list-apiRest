01289c935b4d2bd3627b75963e954f2188864293
Libertad Lorda <132447031+LibertadLorda@users.noreply.github.com>
2023-08-22 15:05:06 +0200

    Update NavbarComponent.vue

--- FRONT/src/components/NavbarComponent.vue
+++ FRONT/src/components/NavbarComponent.vue
@@ -1,7 +1,6 @@
 <script setup>
-
 import { ref, watch } from 'vue';
-
+import { RouterLink } from 'vue-router';
 
 const drawer = ref(false);
 const group = ref(null);
@@ -9,19 +8,19 @@ const group = ref(null);
 const items = [
   {
     title: 'Home',
-    value: '/',
+    route: '/',
   },
   {
-    title: 'Bar',
-    value: 'bar',
+    title: 'Team',
+    route: '/team',
   },
   {
-    title: 'Fizz',
-    value: 'fizz',
+    title: 'Services',
+    route: '/services',
   },
   {
-    title: 'Login',
-    value: '/login',
+    title: 'Contact Us',
+    route: '/contact',
   },
 ];
 
@@ -31,28 +30,55 @@ watch(group, () => {
 </script>
 
 <template>
-      <v-app-bar
-        color="primary"
-        prominent
+  <v-app-bar
+    color="primary"
+    prominent
+  >
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+    <v-toolbar-title class="text-no-wrap" title="Application">Tods List</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <RouterLink to="/favourites"><v-btn variant="text" icon="mdi-heart"></v-btn></RouterLink>
+
+    <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    location="top"
+    temporary
+  >
+
+    <v-list>
+      <RouterLink
+        class="itemList"
+        v-for="item in items"
+        :key="item.title"
+        :to="item.route"
       >
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
-        <v-toolbar-title class="text-no-wrap" title="Application">Tods List</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn variant="text" icon="mdi-heart" v-for="link in links" :key="link.name" :to="link.route"></v-btn>
-
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
-
-      <v-navigation-drawer v-model="drawer" location="top" temporary>        
-        <v-list>
-          <router-link v-for="item in items" :key="items.title" :to="item.value">
-            {{item.title}}
-          </router-link>
-        </v-list>     
-      </v-navigation-drawer>
+        {{ item.title }}
+      </RouterLink>
+    </v-list>
+  </v-navigation-drawer>
 </template>
+
+<style>
+  .itemList {
+    text-decoration: none;
+    color: rgba(54, 157, 178, 1);
+  }
+  .itemList:hover {
+    letter-spacing: 0.15rem;
+    font-weight: bolder;
+  }
+  .v-list{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-left: 1rem;
+  }
+</style>
