850514158ab8bb0aea2ebed9eef7699fe3907959
LibertadLorda <libertadlorda@gmail.com>
2023-08-23 18:15:52 +0200

    CardComponent integred in Form

--- FRONT/src/components/FormComponent.vue
+++ FRONT/src/components/FormComponent.vue
@@ -1,59 +1,58 @@
+<script setup>
+import { ref } from "vue";
+import ProductData from "@/services/ProductDataService";
 
-<script>
+const name = ref("");
+const nameRules = [
+  (v) => !!v || "Frase es obligatoria",
+  (v) => (v && v.length <= 20) || "Name must be less than 20 characters",
+];
+const select = ref(null);
+const items = ["Camiseta", "Sudadera", "Taza", "Botella"];
+const size = ["S", "M", "L", "XL"];
+const colour = [
+  "Blanco",
+  "Negro",
+  "Rojo",
+  "Azul",
+  "Amarillo",
+  "Verde",
+  "Rosa",
+  "Violeta",
+];
+const checkbox = ref(false);
+const response = ref(null);
 
-  export default {
-    data: () => ({
-      name: '',
-      nameRules: [
-        v => !!v || 'Frase es obligatoria',
-        v => (v && v.length <= 20) || 'Name must be less than 20 characters',
-      ],
-      select: null,
-      items: [
-        'Camiseta',
-        'Sudadera',
-        'Taza',
-        'Botella',
-      ],
+const validate = async () => {
+  const { valid } = await $refs.form.validate();
 
-      size:[
-        'S',
-        'M',
-        'L',
-        'XL',
-      ],
-
-      colour:[
-        'Blanco',
-        'Negro',
-        'Rojo',
-        'Azul',
-        'Amarillo',
-        'Verde',
-        'Rosa',
-        'Violeta',
-      ],
-
-      checkbox: false,
-    }),
-
-    methods: {
-      async validate () {
-        const { valid } = await this.$refs.form.validate()
-
-        if (valid) alert('Form is valid')
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
-
-    },
+  if (valid) {
+    try {
+      const result = await ProductData.create({
+        name: name.value,
+        product: select.value,
+        size:
+          select.value === "Camiseta" || select.value === "Sudadera"
+            ? size.value
+            : null,
+        color: colour.value,
+        terms: checkbox.value,
+      });
+      response.value = result.data;
+    } catch (error) {
+      console.log(error);
   }
-</script>
+}
+};
 
+const reset = () => {
+  $refs.form.reset();
+};
+
+const resetValidation = () => {
+  $refs.form.resetValidation();
+};
+</script>
 
 <template>
   <v-sheet width="300" class="mx-auto">
@@ -63,14 +62,14 @@
         v-model="name"
         :counter="20"
         :rules="nameRules"
-        label="Frase mierda here please"
+        label="Escribe una frase!"
         required
       ></v-text-field>
 
       <v-select
         v-model="select"
         :items="items"
-        :rules="[v => !!v || 'Item is required']"
+        :rules="[v => !!v || 'El tipo de producto es necesario']"
         label="Producto"
         required
       ></v-select>
@@ -78,19 +77,19 @@
       <v-select
         v-if="select === 'Camiseta' || select === 'Sudadera'"
         :items="size"
-        :rules="[v => !!v || 'Item is required']"
+        :rules="[v => !!v || 'La talla es necesaria']"
         label="Talla"
       ></v-select>
 
       <v-select
         :items="colour"
-        :rules="[v => !!v || 'Item is required']"
+        :rules="[v => !!v || 'El color es obligatorio']"
         label="Color"
       ></v-select>
 
       <v-checkbox
         v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
+        :rules="[v => !!v || 'Acepta para continuar']"
         label="Do you agree?"
         required
       ></v-checkbox>
@@ -102,7 +101,7 @@
           block
           @click="validate"
         >
-          Validate
+          Validar
         </v-btn>
 
         <v-btn
@@ -111,7 +110,7 @@
           block
           @click="reset"
         >
-          Reset Form
+          Resetea Formulario
         </v-btn>
 
         <v-btn
@@ -120,7 +119,7 @@
           block
           @click="resetValidation"
         >
-          Reset Validation
+          Cancela envío
         </v-btn>
       </div>
     </v-form>
