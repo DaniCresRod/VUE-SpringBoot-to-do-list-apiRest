d35ec8ac82490aae85008a3b8e4439acd367976d
javier <ertoyos@gmail.com>
2023-08-25 15:22:38 +0200

    pluplu

--- FRONT/src/components/ProductCards.vue
+++ FRONT/src/components/ProductCards.vue
@@ -1,34 +1,33 @@
 <template>
-    <v-carousel hide-delimiters>
-      <v-carousel-item
-        v-for="(item,i) in items"
-        :key="i"
-        :src="item.src"
-        cover
-      ></v-carousel-item>
-    </v-carousel>
-    <!-- insertar boton fav traslucido en el centro de la imagen -->
-  </template>
-  
-  <script>
-    export default {
-      data () {
-        return {
-          items: [
-            {
-              src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            },
-            {
-              src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-            },
-            {
-              src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-            },
-            {
-              src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-            },
-          ],
-        }
-      },
-    }
-  </script>
+  <v-carousel hide-delimiters>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+  <!-- insertar boton fav traslucido en el centro de la imagen -->
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          {
+            src: 'src/assets/biscocho_azul.png',
+          },
+          {
+            src: 'src/assets/biscocho_blanca.png',
+          },
+          {
+            src: 'src/assets/biscocho_negra.png',
+          },
+          {
+            src: 'src/assets/blanca_span.png',
+          },
+        ],
+      };
+    },
+  };
+</script>
