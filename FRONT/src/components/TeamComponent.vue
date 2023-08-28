70b3c4f85f423106f8f009aef6ac56ccabcd7061
LibertadLorda <libertadlorda@gmail.com>
2023-08-28 12:53:05 +0200

    ok styles team

--- FRONT/src/components/TeamComponent.vue
+++ FRONT/src/components/TeamComponent.vue
@@ -1,30 +1,28 @@
-<script>
-export default {
-  props: {
+<script setup>
+  import { ref } from 'vue';
+  
+  const props = defineProps({
     imageSrc: String,
     title: String,
     subtitle: String,
     buttonLabel: String,
     content: String,
-  },
-  data() {
-    return {
-      show: false,
-    };
-  },
-};
-</script>
+  });
+  
+  const show = ref(false);
+ 
+  </script>
 
 <template>
-  <v-card class="mx-auto cardTeam" max-width="344" >
+  <v-card class="mx-auto cardTeam" width="225" >
     <v-img :src="imageSrc" height="200px" cover></v-img>
-
+    
     <v-card-title>{{ title }}</v-card-title>
 
     <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
 
     <v-card-actions>
-      <v-btn color="orange-lighten-2" variant="text" @click="show = !show">
+      <v-btn color="primary" variant="text" @click="show = !show">
         {{ buttonLabel }}
       </v-btn>
 
@@ -34,11 +32,24 @@ export default {
     </v-card-actions>
 
     <v-expand-transition>
-      <div v-show="show">
+      <div v-show="show" color="primary" variant="text">
         <v-divider></v-divider>
 
-        <v-card-text>{{ content }}</v-card-text>
+        <v-card-text class="content">{{ content }}</v-card-text>
       </div>
     </v-expand-transition>
   </v-card>
 </template>
+
+<style>
+
+.cardTeam {
+  margin-top: 1rem; 
+}
+
+.content{
+  color: rgba(54, 157, 178, 1);
+  text-align: center;
+}
+
+</style>
