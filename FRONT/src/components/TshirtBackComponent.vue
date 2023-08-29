8675d0f5c1c7ccad7a2cdb86824a60a53e0eaccb
LuLu <lu.luzan@gmail.com>
2023-08-29 14:13:03 +0200

    img_blur

--- FRONT/src/components/TshirtBackComponent.vue
+++ FRONT/src/components/TshirtBackComponent.vue
@@ -1,41 +1,28 @@
 <template>
-    <div class="image-container" @mouseover="activateColor" @mouseleave="deactivateColor">
-      <img :src="imageUrl" :style="imageStyle" alt="Image" />
+    <div class="image-container">
+      <img class="grayscale" src="/public/tshirt_back.jpg" alt="fabrica de camisetas" />
     </div>
   </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        imageUrl: "\public\tshirt_back.jpg",
-        isColorActive: false,
-      };
-    },
-    computed: {
-      imageStyle() {
-        return {
-          filter: this.isColorActive ? "none" : "grayscale(100%)",
-          transform: this.isColorActive ? "scale(1.2)" : "scale(1)",
-        };
-      },
-    },
-    methods: {
-      activateColor() {
-        this.isColorActive = true;
-      },
-      deactivateColor() {
-        this.isColorActive = false;
-      },
-    },
-  };
-  </script>
   
   <style scoped>
   .image-container {
     display: inline-block;
-    transition: transform 0.3s, filter 0.3s;
-    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: transform 0.3s;
+    cursor: none;
+  }
+  
+  .grayscale {
+    width: 100%;
+    height: auto;
+    filter: grayscale(100%);
+    transition: filter 0.3s, transform 0.3s;
+  }
+  
+  .image-container:hover .grayscale {
+    filter: none;
+    transform: scale(1.2);
   }
   </style>
   
